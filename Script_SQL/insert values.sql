eeec53c7e55ba9a59a8f3bfdccd3c928cfbd78df
Luis Andrade <154901202+lluiscase@users.noreply.github.com>
2025-10-27 20:58:07 -0300

    feat:insert paises

--- Script_SQL/insert values.sql
+++ Script_SQL/insert values.sql
@@ -63,6 +63,172 @@ SELECT DISTINCT TRY_CAST(col2 AS nvarchar(100))
 FROM #gapminder
 WHERE col2 IS NOT NULL;
 
+--Insert Paises
+ALTER TABLE Paises
+ALTER COLUMN CodigoISO_Alpha3 CHAR(3) NULL;
+
+UPDATE p
+SET p.CodigoISO_Alpha3 = c.col1
+FROM Paises p
+JOIN #co2 c
+    ON p.NomePais = c.col2;
+
+INSERT INTO Paises (NomePais, ContinenteID)
+VALUES
+('Afghanistan', 2),   -- Asia
+('Albania', 6),       -- Europe
+('Algeria', 3),       -- Africa
+('Angola', 3),        -- Africa
+('Argentina', 5),     -- South America
+('Armenia', 2),       -- Asia
+('Australia', 4),     -- Oceania
+('Austria', 6),       -- Europe
+('Azerbaijan', 2),    -- Asia
+('Bahamas', 1),       -- North America
+('Bahrain', 2),       -- Asia
+('Bangladesh', 2),    -- Asia
+('Barbados', 1),      -- North America
+('Belarus', 6),       -- Europe
+('Belgium', 6),       -- Europe
+('Belize', 1),        -- North America
+('Benin', 3),         -- Africa
+('Bhutan', 2),        -- Asia
+('Bosnia and Herzegovina', 6), -- Europe
+('Botswana', 3),      -- Africa
+('Brazil', 5),        -- South America
+('Bulgaria', 6),      -- Europe
+('Burkina Faso', 3),  -- Africa
+('Burundi', 3),       -- Africa
+('Cambodia', 2),      -- Asia
+('Cameroon', 3),      -- Africa
+('Canada', 1),        -- North America
+('Central African Republic', 3),
+('Chad', 3),
+('Chile', 5),
+('China', 2),
+('Colombia', 5),
+('Comoros', 3),
+('Costa Rica', 1),
+('Croatia', 6),
+('Cuba', 1),
+('Cyprus', 2),
+('Denmark', 6),
+('Djibouti', 3),
+('Dominican Republic', 1),
+('Ecuador', 5),
+('Egypt', 3),
+('El Salvador', 1),
+('Equatorial Guinea', 3),
+('Estonia', 6),
+('Ethiopia', 3),
+('Fiji', 4),
+('Finland', 6),
+('France', 6),
+('Gabon', 3),
+('Gambia', 3),
+('Georgia', 2),
+('Germany', 6),
+('Ghana', 3),
+('Greece', 6),
+('Guatemala', 1),
+('Guinea', 3),
+('Guinea-Bissau', 3),
+('Guyana', 5),
+('Haiti', 1),
+('Honduras', 1),
+('Hungary', 6),
+('Iceland', 6),
+('India', 2),
+('Indonesia', 2),
+('Iraq', 2),
+('Ireland', 6),
+('Israel', 2),
+('Italy', 6),
+('Jamaica', 1),
+('Japan', 2),
+('Jordan', 2),
+('Kazakhstan', 2),
+('Kenya', 3),
+('Kuwait', 2),
+('Latvia', 6),
+('Lebanon', 2),
+('Lesotho', 3),
+('Liberia', 3),
+('Libya', 3),
+('Lithuania', 6),
+('Luxembourg', 6),
+('Madagascar', 3),
+('Malawi', 3),
+('Malaysia', 2),
+('Maldives', 2),
+('Mali', 3),
+('Malta', 6),
+('Mauritania', 3),
+('Mauritius', 3),
+('Mexico', 1),
+('Mongolia', 2),
+('Montenegro', 6),
+('Morocco', 3),
+('Mozambique', 3),
+('Myanmar', 2),
+('Namibia', 3),
+('Nepal', 2),
+('Netherlands', 6),
+('New Zealand', 4),
+('Nicaragua', 1),
+('Niger', 3),
+('Nigeria', 3),
+('North Macedonia', 6),
+('Norway', 6),
+('Oman', 2),
+('Pakistan', 2),
+('Panama', 1),
+('Papua New Guinea', 4),
+('Paraguay', 5),
+('Peru', 5),
+('Philippines', 2),
+('Poland', 6),
+('Portugal', 6),
+('Qatar', 2),
+('Romania', 6),
+('Rwanda', 3),
+('Samoa', 4),
+('Sao Tome and Principe', 3),
+('Saudi Arabia', 2),
+('Senegal', 3),
+('Serbia', 6),
+('Sierra Leone', 3),
+('Singapore', 2),
+('Slovenia', 6),
+('Solomon Islands', 4),
+('South Africa', 3),
+('South Sudan', 3),
+('Spain', 6),
+('Sri Lanka', 2),
+('Sudan', 3),
+('Suriname', 5),
+('Sweden', 6),
+('Switzerland', 6),
+('Tajikistan', 2),
+('Thailand', 2),
+('Timor-Leste', 2),
+('Togo', 3),
+('Tonga', 4),
+('Trinidad and Tobago', 1),
+('Tunisia', 3),
+('Turkmenistan', 2),
+('Uganda', 3),
+('Ukraine', 6),
+('United Arab Emirates', 2),
+('United Kingdom', 6),
+('United States', 1),
+('Uruguay', 5),
+('Uzbekistan', 2),
+('Vanuatu', 4),
+('Yemen', 2),
+('Zambia', 3),
+('Zimbabwe', 3);
+
 select * from #co2; --Tem sua tabela origem chamada EmissoesCO2_Wide
 SELECT * FROM (SELECT ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RN, * FROM #co2)t WHERE rn > 1
 select * from #gapminder;
@@ -73,3 +239,4 @@ select * from #life; --Tem sua tabela origem Expectativa de vida
 select * from ExpectativaVida;
 
 select * from GNIPC EXPECT select * from #gross;
+
